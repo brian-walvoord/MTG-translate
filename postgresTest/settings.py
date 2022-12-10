bf4902af7aaaccf84cc72e846af4d96e0f6ffb71
Brian Walvoord <brian.walvoord@gmail.com>
2022-12-10 11:47:13 +0900

    fix: removed commented key

--- postgresTest/settings.py
+++ postgresTest/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-hz&1tm3oy_q6%+*p897p0mz58iq^#h1!4sp969%%0@!8r(&!c-'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
